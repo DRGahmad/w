9dda9fb85fa7e4baa495b8365949b4b3c087bba0
Glitch (hello-express) <none>
2019-12-15 17:54:02 +0000

    👗📚 Checkpoint ./server.js:4973610/1209 ./config.js:4973610/38

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TOKEN: 'NjU1MDQzNjU1OTExOTk3NDUw.XfOW9w.4zpQ8fIpiSJE81Wvap55zcI1CiM',    
     YT_API_KEY: 'api ',
-    prefix: '$',
-    devs: ['527505679171321856']
+    prefix: '-',
+    devs: ['603456072954544141']
 }
