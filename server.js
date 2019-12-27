bb59e7bf28bee284fef80c31da2dee04aada0e55
Glitch (hello-express) <none>
2019-12-27 13:10:35 +0000

    🎾🌵 Checkpoint ./server.js:4973610/14

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag,messag
 \`#credits <@${URID}> ${P}\`
 لديك 3 دقائق قبل الالغاء.**`));// ahmeD_Hossam
 let P2 = Math.floor(P-(P*(5/100)));// ahmeD_Hossam
-let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$4750\` to <@603456072954544141> **`);// ahmeD_Hossam// ahmeD_Hossam
+let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$1\` to <@603456072954544141> **`);// ahmeD_Hossam// ahmeD_Hossam
 message.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })// ahmeD_Hossam
 .then( collected =>{// ahmeD_Hossam
 let C = 0;let Accs = [];// ahmeD_Hossam
@@ -170,7 +170,7 @@ if(cmd == 'NFA') {
   \`#credits <@${URID}> ${P}\`
   لديك 3 دقائق قبل الالغاء.**`));
   let P2 = Math.floor(P-(P*(5/100)));
-  let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$4750\` to <@603456072954544141> **`)
+  let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$1\` to <@603456072954544141> **`)
   message.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
   .then( collected =>{
   let C = 0;let Accs = [];
