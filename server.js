dc821318fd1911714ec903bd02e2e952be4878fb
Glitch (hello-express) <none>
2020-01-01 19:00:16 +0000

    ⛈👯 Checkpoint ./server.js:2439937/242:4973610/5794

--- server.js
+++ server.js
@@ -42,6 +42,24 @@ const queue = new Map();
 var table = require('table').table
 const Discord = require('discord.js');
  
+console.log("==================================")
+console.log("1")
+console.log("2")
+console.log("3")
+console.log("=========> Bot Online <=========")
+console.log("========> TestBot <========")
+console.log("=======> Token Bot **** <=======")
+console.log("3")
+console.log("2")
+console.log("1")
+console.log("====================================")
+console.log("Bot Online 24/7");
+
+
+
+
+
+
  //كود للتجربة 
 
 client.on('message', message =>{
@@ -85,6 +103,9 @@ client.on('message', async message => {
 
         }
 });
+
+//kk
+const ticketInfos = [];//dont delete
 client.on('message', async message => {
       const db = require("quick.db");
  let args = message.content.split(' ').slice(1).join(' ');
@@ -96,6 +117,7 @@ client.on('message', async message => {
                 if(bl === "on") return message.channel.send("**you're black listed from tickets system !!**")
 
   if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.channel.send(`**Error** :octagonal_sign:\nI Don\'t have MANAGE_CHANNELS Permission`)
+    if(!ticketInfos[message.guild.id+message.author.id]) ticketInfos[]
  let log = message.guild.channels.find("name", "tickets-log");
  let args = message.content.split(' ').slice(1).join(' ');
  let support = message.guild.roles.find("name","Support");
@@ -267,90 +289,4 @@ if(log) return log.send(gtg);
  }
  
  });
-
-console.log("==================================")
-console.log("1")
-console.log("2")
-console.log("3")
-console.log("=========> Bot Online <=========")
-console.log("========> TestBot <========")
-console.log("=======> Token Bot **** <=======")
-console.log("3")
-console.log("2")
-console.log("1")
-console.log("====================================")
-console.log("Bot Online 24/7");
-
-
-
-
-let sfa = JSON.parse(fs.readFileSync('./sfa.json', 'utf8')); // الملف الي بتحط به الحسابات الفل داتا
-let nfa = JSON.parse(fs.readFileSync('./nfa.json', 'utf8')); // الملف الي بتحط به الحسابات العاديه
-let SFAP = 8500;/*سعر الحساب الواحد الفل داتا*/let NFAP = 250;/*سعر الحساب الواحد العادي*/let URID = '432231487916736542'//مين بيتحوله الكريديت// ahmeD_Hossam
-client.on('message',async message => {// ahmeD_Hossam
-if(message == prefix+'stock') {let ahmed = 0;let hossam = 0;// ahmeD_Hossam
-  sfa.forEach(acc =>{if(!acc.email) return;ahmed++;});// ahmeD_Hossam
-  nfa.forEach(acc =>{if(!acc.email) return;hossam++;});// ahmeD_Hossam // i will kill you soon ok ? xD
-  message.channel.send(new Discord.RichEmbed().setTitle('💵 متجر اوريو 💵')// ahmeD_Hossam
-  .addField('**[SFA | فل داتا] > **',`**${ahmed} Account(s)**`,true)  .addField('**[NFA | عادي] > **',`**${hossam} Account(s)  **`,true).setColor('GREEN')// ahmeD_Hossam
-.addField('**هل تعلم ؟**',`**\`[SFA]\` > فل داتا - حساب يمكنك اللعب وتغيير الاسم والباسوورد والسكن
-\`[NFA]\` > العادي = حساب لا يمكنك تغيير اي شيئ فيه, للعب فقط**`)// ahmeD_Hossam
-.addField('**الاسعار**',`\`[1 SFA] > 8500 Credits ProBot\` \n \`[1 NFA] > 250 Credits ProBot\``)// ahmeD_Hossam
-.setFooter(`$buy [sfa/nfa] [الحسابات لا يوجد عليها ضمان | لشراء حساب الرجاء كتابة الأمر التالي [الكمية`) )}// ahmeD_Hossam
-if(message.content.startsWith(prefix+'buy')){// ahmeD_Hossam
-let cmd = message.content.split(" ")[1];// ahmeD_Hossam
-let args = message.content.split(" ")[2];// ahmeD_Hossam
-if(!cmd || !args || isNaN(args)) return message.channel.send(`**استخدام خاطئ ّ! \n Correct Usage Example: $buy SFA 1**`);// ahmeD_Hossam
-if(cmd == 'sfa') {// ahmeD_Hossam
-  let ahmed = 0
-  sfa.forEach(acc =>{if(!acc.email) return;ahmed++;});
-      if(ahmed < 1) return message.channel.send("لا يوجد حسابات")
-  if(ahmed < args) return message.channel.send("لا يوجد حسابات كافية") // 
-message.author.send('✅ Nothing.. Just Check  If Your DM open or no').then(()=>{// ahmeD_Hossam
-let P = Math.floor(args*(SFAP))// ahmeD_Hossam
-message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag,message.author.avatarURL).setColor('#918383')// ahmeD_Hossam
-.setDescription(`**اكتب الامر التالي لأكمال عمليه الشراء
-#credits <@603456072954544141> ${P}
-لديك 3 دقائق قبل الالغاء.**`));// ahmeD_Hossam
-let P2 = Math.floor(P-(P*(5/100)));// ahmeD_Hossam
-let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$${P2}\` to <@603456072954544141> **`);// ahmeD_Hossam// ahmeD_Hossam
-message.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })// ahmeD_Hossam
-.then( collected =>{// ahmeD_Hossam
-let C = 0;let Accs = [];// ahmeD_Hossam
-sfa.forEach(acc =>{if(!acc.email) return;if(C == args)return;;Accs.push(`Email: ${acc.email} | pass: ${acc.pass}`);C++;// ahmeD_Hossam// ahmeD_Hossam
-delete acc.email;delete acc.pass;// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam
-fs.writeFile("./sfa.json", JSON.stringify(sfa), (err) => {if (err) console.error(err)})// ahmeD_Hossam// ahmeD_Hossam// ahmeD_Hossam
-});message.channel.send('**Done,,\nNow Check Your DM**!')
-message.author.send(`Your Accs :)\`\`\`json\n${Accs.join("\n")}\n\`\`\`سيتم خذف الرساله بعد 5 دقائق !`).then(M =>M.delete(5*60*1000))
-});}).catch(err=>{return message.channel.send('**:x: Please Open Your DM**!')}) }
-if(cmd == 'nfa') {
-  let hossam = 0;
-  sfa.forEach(acc =>{if(!acc.email) return;hossam++;})
-  if(hossam < 1) return message.channel.send("لا يوجد حسابات")
-  if(hossam < args) return message.channel.send("لا يوجد حسابات كافية")
-  message.author.send('✅ Nothing.. Just Check If Your DM open or no').then(()=>{
-  let P = Math.floor(args*(NFAP))// ahmeD_Hossam
-  message.channel.send(new Discord.RichEmbed().setAuthor(message.author.tag,message.author.avatarURL).setColor('#918383')
-  .setDescription(`**اكتب الامر التالي لأكمال عمليه الشراء
-  Ex : #credits <@603456072954544141> ${P2}
-  لديك 3 دقائق قبل الالغاء.**`));
-  let P2 = Math.floor(P-(P*(5/100)));
-  let filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`**:moneybag: | ${message.author.username}, has transferred \`$${P2}\` to <@603456072954544141> **  `)
-  message.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
-  .then( collected =>{
-  let C = 0;let Accs = [];
-  nfa.forEach(acc =>{if(!acc.email) return;if(C == args)return;;Accs.push(`Email: ${acc.email} | pass: ${acc.pass}`);C++;  delete acc.email;delete acc.pass;
-  fs.writeFile("./nfa.json", JSON.stringify(nfa), (err) => {if (err) console.error(err)})
-  });message.channel.send('**Done,,\nNow Check Your DM**!')
-  message.author.send(`Your Accs :)\`\`\`json\n${Accs.join("\n")}\n\`\`\`سيتم خذف الرساله بعد 5 دقائق !`).then(M =>M.delete(5*60*1000))
-  });}).catch(err=>{return message.channel.send('**:x: Please Open Your DM**!')}) } }
-})
-
-
-  
-
-
-
-
-
 
