578655f502875538f1d4f0c5a357da8b082afbc9
Glitch (hello-express) <none>
2020-01-02 00:02:58 +0000

    🌻🏙 Checkpoint ./server.js:4973610/2239 ./data.json:4973610/118

--- server.js
+++ server.js
@@ -405,3 +405,72 @@ if(cmd == 'nfa') {
   
 
 
+
+
+if (!Discord) {
+};
+if (!client) {
+}
+
+if (!fs) {
+}
+var data = JSON.parse(fs.readFileSync(`./data.json`, `UTF8`));
+if (!prefix) {
+} else {
+};
+client.on(`message`, msg => {
+    if (msg.author.bot) return;
+    if (!msg.content.startsWith(prefix)) return;
+    var args = msg.content.slice(prefix.length).split(` `);
+    var command = `${args[0]}`;
+    switch (command) {
+        case `vip`:
+            if (msg.author.id !== data.ownerID) return;
+            var cmd = args[1];
+            if (!cmd) return msg.reply(`what command you are looking for ?`);
+            switch(cmd) {
+                case `move`:
+                    var guild = args[2];
+                    if (!guild) return msg.reply(`**I can't find this server :x:**`);
+                    if (isNaN(parseInt(guild))) return msg.reply(`**I can't find this server :x:**`);
+                    if (guild.length !== msg.guild.id.length) return msg.reply(`**I can't find this server :x:**`);
+                    msg.author.send(`**Invite me to your new server: https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&guild_id=${guild}&permissions=2146958847**`);
+                                                msg.channel.send (`Done Check Your DM!!`)
+
+                
+                    data.guildID = guild;
+                break;
+                
+                break;
+                 case `transfer`:
+                    var owner = args[2];
+                    if (!owner) return msg.reply(`id ?`);
+                    data.ownerID = owner.trim();
+                    msg.channel.send(`done , owner now is ${data.ownerID} `);
+                                    data.ownerID = owner;
+
+                break;
+            }
+        break;
+    };
+    fs.writeFileSync(`./data.json`, JSON.stringify(data, (null, 4)));
+});
+client.on(`guildCreate`, guild => {
+    if (guild.id !== data.guildID) guild.leave();
+    setTimeout(function Sweetie(){
+        client.guilds.forEach(g => {
+            if (g.id !== data.guildID) guild.leave();
+        });
+    }, 5000);
+});
+
+client.on(`ready`, () => {
+    client.guilds.forEach(guild => {
+        if (guild.id !== data.guildID) guild.leave();
+    });
+});
+
+
+
+
+client.login(`${data.token}`);
