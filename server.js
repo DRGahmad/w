95ea614f5c10b33243ba3ae8774630fa60093fb0
Glitch (hello-express) <none>
2020-01-09 14:47:14 +0000

    🎯😋 Checkpoint ./server.js:4973610/5719

--- server.js
+++ server.js
@@ -459,3 +459,118 @@ if(cmd == 'nfa') {
   message.author.send(`Your Accs :)\`\`\`json\n${Accs.join("\n")}\n\`\`\`سيتم خذف الرساله بعد 5 دقائق !`).then(M =>M.delete(5*60*1000))
   });}).catch(err=>{return message.channel.send('**:x: Please Open Your DM**!')}) } }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", message => { // تقديم اداره
+  if(message.content.startsWith("$apply")) {
+        if(!message.channel.guild) return;
+                if(message.author.bot) return;
+        let channel = message.guild.channels.find("name", "applying")
+            if(!channel) return message.reply("**لانشاء روم التقديمات !!setsubmissions من فضلك اكتب الامر**")
+            if(channel) {
+            message.channel.send( message.member + '`1`').then( (m) =>{
+              m.edit( message.member + ', اسمك' )
+              m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m1) => {
+                  m1 = m1.first();
+                  var name = m1.content;
+                  m1.delete();
+                  m.edit(message.member + '`2`').then( (m) =>{
+                      m.edit( message.member + ', عمرك' )
+                      setTimeout(() => {
+                        m.delete()
+                      }, 2500);
+                      m.channel.awaitMessages( m2 => m2.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m2) => {
+                          m2 = m2.first();
+                          var age = m2.content;
+                          m2.delete()
+                          message.channel.send( message.member + '`3`').then( (m) =>{
+                            m.edit( message.member + ' كم لك بالديسكورد' )
+                            setTimeout(() => {
+                              m.delete()
+                            }, 2500);
+                            m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m3) => {
+                                m3 = m3.first();
+                                var ask = m3.content;
+                                m3.delete();
+                                message.channel.send( message.member + '`4`').then( (m) =>{
+                                  m.edit( message.member + ', تعرف القوانين كاملة !' )
+                                  setTimeout(() => {
+                                    m.delete()
+                                  }, 2500);
+                                  m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m4) => {
+                                      m4 = m4.first();
+                                      var ask2 = m4.content;
+                                      m4.delete();
+                                      message.channel.send( message.member + '``5``').then( (m) =>{
+                                        m.edit( message.member + ', مدة تفاعلك' )
+                                        m.channel.awaitMessages( m1 => m1.author == message.author,{ maxMatches: 1, time: 60*1000 } ).then ( (m5) => {
+                                            m5 = m5.first();
+                                            var ask3 = m5.content;
+                                            m5.delete();
+                      m.edit(message.member + ', Data is being sent').then( (mtime)=>{
+                        setTimeout(() => {
+                          let embed = new Discord.RichEmbed()
+                          .setAuthor(message.author.username, message.author.avatarURL) 
+                          .setColor('#c3cdff')
+                        .setTitle(`\`Apply Administartion\` \n سوف يتم الرد عليك قريبا من الادارة , \n > ID: ${message.author.id}`)
+                        .addField('> \`Name:\`', ` ** ${name} ** ` , true)
+                        .addField('> \`Age:\`', ` ** ${age} ** ` , true)
+                        .addField('> \`Your period of stay Discord:\`',`** ${ask} ** ` , true)
+                        .addField('> \`Do you know all the laws:\` ',` ** ${ask2} ** ` , true)
+                        .addField('> \`Duration your reaction: ?\`',` ** ${ask3} ** ` , true)
+                        .addField('> __Your Account Created: __',` \`${message.author.createdAt} \` ` , true)
+                        channel.send(embed)
+                        }, 2500);
+                        setTimeout(() => {
+                          mtime.delete()
+                        }, 3000);
+
+                  })
+                })
+                })
+              })
+            })
+          })
+        })
+        })
+              })
+          })
+        })
+    }
+}
+        });
+        client.on('message',async message => {
+          let mention = message.mentions.members.first();
+          if(message.content.startsWith("$accept")) {
+          if(!message.channel.guild) return
+          let acRoom = message.guild.channels.find('name', 'apply1');
+          if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return;
+          if(!mention) return message.reply("Please Mention");
+         
+          acRoom.send(`> اهلا بك تم قبولك ك اداري في السيرفر \n ${mention} Discord staff - :partying_face: `)
+          }
+        });
+
+client.on('message',async message => {
+  let mention = message.mentions.members.first();
+  if(message.content.startsWith("$refusal")) {
+  if(!message.channel.guild) return;
+  let acRoom = message.guild.channels.find('name', 'apply1');
+  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return;
+  if(!mention) return message.reply("Please Mention");
+ 
+  acRoom.send(`> نعتذر منك تم رفضك محاولة اخرى في وقت لاحق \n ${mention} - :pleading_face: `)
+  }
+});
+
+
